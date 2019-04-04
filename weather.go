17b2b4c57a085d6f2245d56f2aa2056d566b7fe8
Vikrant Gajria <vikrantgajria@gmail.com>
2019-04-04 23:43:09 +0530

    Get date from response

--- weather.go
+++ weather.go
@@ -2,9 +2,27 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// Weather () - displays the weather based on the queryResult
-func Weather(res NLPResponse) {
+// Weather - displays the weather based on the queryResult
+func Weather(res NLPResponse) error {
 	fmt.Println(res.Response)
+
+	// First we get the time from the response
+	var target time.Time
+	if res.Entities["date"] != "" {
+		var err error
+		target, err = time.Parse(time.RFC3339, res.Entities["date"])
+		if err != nil {
+			return err
+		}
+	} else {
+		target = time.Now()
+	}
+	fmt.Println(target)
+
+	// TODO: Make a POST request to Openweather API
+
+	return nil
 }
